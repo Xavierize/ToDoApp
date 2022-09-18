42331549556300e6a956d433cf0839cd339cf203
Xavierize <xkajack@gmail.com>
2022-09-18 15:18:53 -0400

    Added JS functions

--- Todo.js
+++ Todo.js
@@ -2,6 +2,7 @@ let addToDoButton = document.getElementById("addToDo");
 let ToDoContainer = document.getElementById("todoContainer");
 let inputField = document.getElementById("inputField");
 
+//Fix the add.EventListener bug//
 addToDoButton.addEventListener("click", function(){
     var paragraph = document.createElement("p")
     paragraph.innerText = inputField.value;
